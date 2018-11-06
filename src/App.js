862524afaefad541c34ef27aca31017f79de7ee7
tom lee <tdroid99@gmail.com>
2018-11-06 11:39:59 -0700

    working dismiss button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const list = [
@@ -21,19 +20,42 @@ const list = [
 ];
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
   render() {
     return (
       <div className="App">
-        {list.map((item) => {
+        {this.state.list.map((item)=> {
           return (
-            <div>
+            <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
               </span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
-              </div>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type = "button"
+                > 
+                    Dismiss
+                </button>
+              </span>
+            </div>
           );
         })}
       </div>
